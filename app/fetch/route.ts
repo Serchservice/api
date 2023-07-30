ed6b500722291998efcde447307179d71e25629e
Mervo <officialmervo@gmail.com>
2023-07-31 00:03:04 +0100

    Fixed env

--- app/fetch/route.ts
+++ app/fetch/route.ts
@@ -1,5 +1,5 @@
 import { NextResponse } from "next/server";
-// api.serchservice.com/fetch?keys=google
+// api.serchservice.com/fetch?key=google
 
 export async function GET(request: Request,
     {params} : {
